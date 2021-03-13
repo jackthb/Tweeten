33fa928a273c6182dcf4937591f17b3826ad3616
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-03-13 14:33:35 +0000

    vv

--- components/featureHighlight.js
+++ components/featureHighlight.js
@@ -2,7 +2,9 @@ export default function FeatureHighlight({ sizeBig, img, imgPosition, contClass,
     return (
         <div className={`feature ${imgPosition === "left" ? "a-left" : ""}`}>
             <div className="inner container">
-                <div className={`img-cont ${sizeBig ? "big" : ""} ${contClass ? contClass : ""}`}><img className={`shadow ${imgClass ? imgClass : ""}`} src={`./images/${img}`} alt={imgAlt}/></div>
+                <div className={`img-cont ${sizeBig ? "big" : ""} ${contClass ? contClass : ""}`}>
+                    <img className={`${imgClass ? imgClass : ""}`} src={`./images/${img}`} alt={imgAlt}/>
+                </div>
                 <div className="meta">
                     <h2>{title}</h2>
                     <p>{text}</p>
