7ab1ff3e005f0120255363639462f06d94e4ee6a
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-03-16 19:36:46 +0000

    added buttons for releases

--- components/releaseHighlight.js
+++ components/releaseHighlight.js
@@ -1,7 +1,6 @@
 import ReactMarkdown from 'react-markdown';
 
 const releaseHighlight = ({ release }) => {
-  const dateOptions = {};
   return (
     <li>
       <time className='tweeten_tmtime'>
@@ -11,15 +10,29 @@ const releaseHighlight = ({ release }) => {
       <div className='tweeten_tmicon tweeten_tmicon-phone'></div>
       <div className='tweeten_tmlabel'>
         <h2>{release.name}</h2>
-        <p>
-          <ReactMarkdown>{release.body}</ReactMarkdown>
-        </p>
+        <ReactMarkdown>{release.body}</ReactMarkdown>
+        <div className='download-release'>
+          <span>Download now:</span>
+          <a
+            href={`https://github.com/MehediH/Tweeten/releases/download/${release.tag_name}/TweetenSetup.exe`}
+            className='dl-link'
+            target='_blank'
+            title='Download Tweeten for your Windows 7, 8, or 10 PC, today.'
+          >
+            Windows (64-bit)
+          </a>
+          <a
+            href={`https://github.com/MehediH/Tweeten/releases/download/${release.tag_name}/tweeten-darwin-x64.zip`}
+            className='dl-link'
+            target='_blank'
+            title='Download Tweeten for your Mac, today.'
+          >
+            macOS
+          </a>
+        </div>
       </div>
     </li>
   );
 };
 
 export default releaseHighlight;
-/* <div className='timeline releases container'>
-<h2 className='indicator'>Loading...</h2>
-<ul className='tweeten-release'>hi</ul></div> */
